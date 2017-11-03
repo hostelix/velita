8fa8660509364f847b699fe654b8e54325efc05a
Israel Lugo <hostelixisrael@gmail.com>
2017-11-02 22:23:30 -0400

    [MOD] add package godotenv for read .env file

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
+
 package main
 
 import (
-	"fmt"
+    "github.com/joho/godotenv"
+    "log"
+    "os"
+    "fmt"
 )
 
-func main(){
-	fmt.Println("Simulation Velita II")
+func main() {
+ 	err := godotenv.Load()
+
+  	if err != nil {
+    	log.Fatal("Error loading .env file")
+  	}
+
+  	title_app := os.Getenv("TITLE_APP")
+  	
+  	fmt.Println(title_app)
 }
